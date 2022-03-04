f8d72cc0d85437d44c94f54676ab5ae6edf3a498
Tomasz Dobrowolski <jarv@monochrome.pl>
2022-03-04 00:43:44 +0000

    added flask and directory structure

--- main.py
+++ main.py
@@ -1,31 +1,53 @@
 import os
+import flask
 import json
 import discord
+
 import random
 import datetime
-from replit import db
+from replit import db, web
+
+#web interface code
+
+app = flask.Flask(__name__)
+users = web.UserStore()
+
+@app.route("/")
+@web.authenticated
+def index():
+    hits = users.current.get("hits", 0) + 1
+    users.current["hits"] = hits
+    return f"You have visited this page {hits} times"
+
+web.run(app)
+
+# discors bot code
 
 client = discord.Client()
 
 def is_me(message):
-  return message.author == client.user
+    return message.author == client.user
+
 
 def is_command(message):
-  return message.content.startswith('!')
+    return message.content.startswith('!')
+
 
 @client.event
 async def on_ready():
-  print('connected as {0.user}'.format(client))
+    print('connected as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
-  # not replying to ourselves
-  if message.author == client.user:
-    return
+    # not replying to ourselves
+    if message.author == client.user:
+        return
 
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
-
-client.run(os.environ['TOKEN'])
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
     
+
+
+client.run(os.environ['TOKEN'])
